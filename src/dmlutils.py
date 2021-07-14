56cb06edd087ca07b9dbf51d193ba6b0f19bd297
Alessandro Martinello <martinello.alessandro@gmail.com>
2021-07-14 12:41:03 +0200

    predict methods allow multiclass

--- src/dmlutils.py
+++ src/dmlutils.py
@@ -44,43 +44,51 @@ class DML_diagnostics:
         res = []
         for k, (train, test) in enumerate(self.model.cv.split(controls)):
             pred_func = self.model.models_t[0][k].predict_proba if predict_proba else self.model.models_t[0][k].predict
-            res.append(pd.DataFrame(
-                pred_func(self._select(controls, test)).reshape(-1, 1)[:, -1],
-                columns=['T_pred'], index=test)
-            )
+            if predict_proba:
+                res.append(pd.DataFrame(
+                    pred_func(self._select(controls, test))[:, -1].reshape(-1, 1),
+                    columns=['T_pred'], index=test)
+                )
+            else:
+                res.append(pd.DataFrame(
+                    pred_func(self._select(controls, test)),
+                    columns=['T_pred'], index=test)
+                )
         
         return pd.concat(res, axis=0).sort_index()
 
-    def get_score_T(self, scoring_function, W, T, predict_proba='auto'):
+    def predict_y(self, W=None, X=None, predict_proba='auto'):
         """
-        Computes the first stage `scoring_function` from a DML model for the
-        treatment variable, where `scoring_function` is a sklearn scoring
-        function. Assumes that the `cv_splitter` used in the training of the
-        model has been the same used when instantiating the `DML_diagnostics`
-        class
         """
-        scores = []
-        for k, (train, test) in enumerate(self.model.cv.split(W)):
-            if predict_proba==True or (predict_proba == 'auto' and (self.model.discrete_treatment==True)):
-                pred = self.model.models_t[0][k].predict_proba(self._select(W, test))[:, 1]
+        if W is not None:
+            if X is not None:
+                controls = np.hstack([X, W])
             else:
-                pred = self.model.models_t[0][k].predict(self._select(W, test))
-            scores += [scoring_function(self._select(T, test), pred)]
-        return np.mean(scores)
+                controls = W
+        else:
+            if X is not None:
+                controls = X
+            else:
+                controls = None
+        if predict_proba=='auto':
+            predict_proba = False
 
-    def get_score_y(self, scoring_function, W, T):
-        """
-        Computes the first stage `scoring_function` from a DML model for the
-        treatment variable, where `scoring_function` is a sklearn scoring
-        function. Assumes that the `cv_splitter` used in the training of the
-        model has been the same used when instantiating the `DML_diagnostics`
-        class
-        """
-        scores = []
-        for k, (train, test) in enumerate(self.model.cv.split(W)):
-            pred = self.model.models_y[0][k].predict(self._select(W, test))
-            scores += [scoring_function(self._select(T, test), pred)]
-        return np.mean(scores)
+        res = []
+        for k, (train, test) in enumerate(self.model.cv.split(controls)):
+            pred_func = self.model.models_y[0][k].predict_proba if predict_proba else self.model.models_y[0][k].predict
+            if predict_proba:
+                res.append(pd.DataFrame(
+                    pred_func(self._select(controls, test))[:, -1].reshape(-1, 1),
+                    columns=['y_pred'], index=test)
+                )
+            else:
+                res.append(pd.DataFrame(
+                    pred_func(self._select(controls, test)),
+                    columns=['y_pred'], index=test)
+                )
+
+        
+        return pd.concat(res, axis=0).sort_index()
 
     def first_stage_residuals(self, y, T, W=None, X=None):
         """
